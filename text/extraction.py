a2bedc713b2c5a9befd6260f762f68863d802c19
Daniel.Montivero <montivero.daniel.e@gmail.com>
2022-08-08 15:09:59 -0300

    Update parameter to text.extraction and support to japanese

--- text/extraction.py
+++ text/extraction.py
@@ -5,30 +5,54 @@ import re
 import numpy as np
 import xml.etree.ElementTree as ET
 
+import util
 from config import PATH_TO_TESSERACT, OUTPUT_DIR, BUBBLE_SUFFIX_FOLDER
 from PIL import Image
 from pytesseract import pytesseract
 from xml.dom import minidom
 
-from util import get_filename_and_ext, allowed_file
+from manga_ocr import MangaOcr
+
+from util import get_filename_and_ext, allowed_file, Singleton
 
 
-def _image_to_text(image_path):
-    pytesseract.tesseract_cmd = PATH_TO_TESSERACT
-    image_buffer = Image.open(image_path)
-    text = pytesseract.image_to_string(image_buffer, lang='eng', config='--psm 3')
+class MangaOcrInstance(metaclass=Singleton):
+
+    def __init__(self):
+        self.m_ocr = MangaOcr()
+
+    def get_ocr(self):
+        return self.m_ocr
+
+
+def _image_to_text(image_path, lang="en"):
+    """ Image to Text
+
+    :param image_path: Image location
+    :param lang: language on image [en|jpn]
+    :return: Text from image
+    """
+    text = ''
+    if lang is None or lang == "en":
+        image_buffer = Image.open(image_path)
+        pytesseract.tesseract_cmd = PATH_TO_TESSERACT
+        text = pytesseract.image_to_string(image_buffer, lang='eng', config='--psm 3')
+    elif lang == "jpn":
+        m_ocr = MangaOcrInstance().get_ocr()
+        text = m_ocr(image_path)
 
     file_name = get_filename_and_ext(image_path)["filename"]
 
     image_data = {"id": file_name}
-    if text is not None:
-        image_data["text"] = text.replace("\n", " ").strip()
+    if text is not None and len(text) > 0:
+        replace = text.replace("\n", " ")
+        image_data["text"] = " ".join(replace.split())
 
     print("Processed file " + file_name)
     return image_data
 
 
-def _remove_noise(image_path):
+def _remove_noise(image_path, force_clean=False):
     if re.match('.+__clean\\.', image_path):
         return image_path
 
@@ -36,7 +60,7 @@ def _remove_noise(image_path):
     file_parts = os.path.splitext(image_path)
     image_path_clean = file_parts[0] + "__clean" + file_data["ext"]
 
-    if not os.path.exists(image_path_clean):
+    if not os.path.exists(image_path_clean) or force_clean:
         np_img_buff = np.fromfile(image_path, np.uint8)
         img = cv2.imdecode(np_img_buff, cv2.IMREAD_COLOR)
         img = cv2.resize(img, (0, 0), fx=1.75, fy=1.75)
@@ -49,7 +73,7 @@ def _remove_noise(image_path):
     return image_path_clean
 
 
-def _folder_image_to_text_file(image_path_folder, output_format="text", preserve_id=True):
+def _folder_image_to_text_file(image_path_folder, output_format="text", preserve_id=True, lang="en", force=False):
     walks = os.walk(image_path_folder)
 
     output_file_name = {
@@ -71,8 +95,8 @@ def _folder_image_to_text_file(image_path_folder, output_format="text", preserve
     image_to_text_data = []
     image_text_data_obj = {"data": image_to_text_data}
     for image_path in files:
-        clean_image_path = _remove_noise(image_path)
-        image_text_data = _image_to_text(clean_image_path)
+        clean_image_path = _remove_noise(image_path, force_clean=force)
+        image_text_data = _image_to_text(clean_image_path, lang=lang)
         image_to_text_data.append(image_text_data)
 
     if output_format == "text":
@@ -133,8 +157,14 @@ def _create_xliff_str(image_text_data_obj, preverse_id=True):
         .toprettyxml(indent="   ", )
 
 
-def create_translation_files(output_format="text", preserve_id=True):
-    text_folders = [x[0] for x in os.walk(OUTPUT_DIR) if re.match(".+" + BUBBLE_SUFFIX_FOLDER, x[0])]
+def create_translation_files(output_format="text", lang="en", preserve_id=True, file=None, force=False):
+    if file is None:
+        text_folders = [x[0] for x in os.walk(OUTPUT_DIR) if re.match(".+" + BUBBLE_SUFFIX_FOLDER, x[0])]
+    else:
+        file_name_without_extension = util.get_filename_and_ext(file)["filename"]
+        text_folders = [x[0] for x in os.walk(OUTPUT_DIR)
+                        if re.match(".+" + file_name_without_extension + BUBBLE_SUFFIX_FOLDER, x[0])]
 
     for text_folder in text_folders:
-        _folder_image_to_text_file(text_folder, output_format, preserve_id)
+        _folder_image_to_text_file(image_path_folder=text_folder, output_format=output_format,
+                                   preserve_id=preserve_id, lang=lang, force=force)
